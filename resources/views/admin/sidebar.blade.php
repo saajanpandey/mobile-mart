77e4b4f1b218035261171f4a1196295bd3f69678
Saajan Raj Pandey <saajan.pandey@ekbana.info>
2022-02-27 08:06:15 +0545

    added image in sodebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -25,9 +25,10 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin.dash')}}">
-                {{-- <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div> --}}
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <img alt="" src="{{ asset('/frontend/img/logo/logo.png') }}"
+                    style="width: 70px;  height: 70px;margin: -18px;">
+                </div>
                 <div class="sidebar-brand-text mx-3">Mobile Mart</div>
             </a>
 
